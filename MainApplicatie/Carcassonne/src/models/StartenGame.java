36095c1ccef7ec969ff3fc598d6820e3c44fae82
Justin Moor <justin-804@hotmail.com>
2017-05-23 12:02:46 +0200

    menu functies

---
+++ MainApplicatie/Carcassonne/src/models/StartenGame.java
@@ -0,0 +1,24 @@
+package models;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+
+public class StartenGame extends Scene {
+
+	Pane pane;
+
+	public StartenGame() {
+		
+		super(new Pane(), 400, 400);
+		pane = (Pane) this.getRoot();
+		init(pane);
+	}
+	
+	private void init (Pane pane){
+		Button start = new Button("Start");
+		pane.getChildren().add(start);
+	}
+}
