c461ce762e834383cafcad6d3501f598f552c0b6
Testman3 <testman3@hotmail.com>
2017-05-30 18:18:55 +0200

    comments toegevoegd bij een aantal functies in de interface

--- Carcassonne_Server/src/Models/RMIInterface.java
+++ Carcassonne_Server/src/Models/RMIInterface.java
@@ -7,9 +7,32 @@ import java.util.ArrayList;
 public interface RMIInterface extends Remote {
 
 
+	/**
+	 * Deze functie zorgt ervoor dat er een nieuw spelerobject wordt aangemaakt met de meegegeven naam, 
+	 * die vervolgens in een arraylist op de server wordt geplaatst
+	 * @param naam
+	 * @throws RemoteException
+	 */
 	public void addPlayer(String naam) throws RemoteException;
+	/**
+	 * Deze functie zorgt ervoor dat het spelerobject in de arraylist dat overeenkomt met de
+	 * meegegeven naam uit de arraylist wordt verwijderd.
+	 * @param naam
+	 * @throws RemoteException
+	 */
 	public void removePlayer(String naam) throws RemoteException;
+	/**
+	 * Deze functie controleert of de meegegeven naam reeds voorkomt in de arraylist met spelers
+	 * @param naam
+	 * @return true / false
+	 * @throws RemoteException
+	 */
 	public boolean checkContains(String naam) throws RemoteException;
+	/**
+	 * Deze functie geeft een ArrayList terug met de namen van alle spelers aanwezig in de ArrayList op de server
+	 * @return ArrayList<String>
+	 * @throws RemoteException
+	 */
 	public ArrayList<String> getPlayerList() throws RemoteException;
 
 }
