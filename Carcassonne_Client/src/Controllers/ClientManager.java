3ca7eab8b0abbebd3b264a0f8ac06fb512cd79b2
Testman3 <testman3@hotmail.com>
2017-06-02 09:18:24 +0200

    Kleine aanpassing

--- Carcassonne_Client/src/Controllers/ClientManager.java
+++ Carcassonne_Client/src/Controllers/ClientManager.java
@@ -6,7 +6,7 @@ import javafx.stage.Stage;
 public class ClientManager extends Application {
 
 	public static void main(String[] args) {
-		
+		//Dit is de main, de launch is niet wit!
 		launch(args);
 	}
 
