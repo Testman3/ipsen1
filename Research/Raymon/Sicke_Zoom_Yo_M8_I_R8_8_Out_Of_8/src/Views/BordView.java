9e7901525c8f32a3ddd3013d2c5b475a593a3faa
Testman3 <testman3@hotmail.com>
2017-06-09 10:38:53 +0200

    BordView added

--- Research/Raymon/Sicke_Zoom_Yo_M8_I_R8_8_Out_Of_8/src/Views/BordView.java
+++ Research/Raymon/Sicke_Zoom_Yo_M8_I_R8_8_Out_Of_8/src/Views/BordView.java
@@ -1,15 +1,28 @@
 package Views;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class BordView extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
+		BorderPane mainPane = new BorderPane();
+		Scene mainScene = new Scene(mainPane, 1280, 720);
+		mainPane.getStylesheets().add("Views/markup.css");
+		mainPane.setId("hallo");
+		
+
+
+
+
+		primaryStage.setScene(mainScene);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
-		// write your code here
+		launch(args);
 	}
 }
