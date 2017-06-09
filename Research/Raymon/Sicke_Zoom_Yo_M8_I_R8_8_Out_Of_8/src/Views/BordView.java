4e4c07e80e169d7344dbf6f4ad756b880c16a21b
Testman3 <testman3@hotmail.com>
2017-06-09 11:16:28 +0200

    Fucked up kaarten

--- Research/Raymon/Sicke_Zoom_Yo_M8_I_R8_8_Out_Of_8/src/Views/BordView.java
+++ Research/Raymon/Sicke_Zoom_Yo_M8_I_R8_8_Out_Of_8/src/Views/BordView.java
@@ -3,6 +3,8 @@ package Views;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class BordView extends Application{
@@ -10,14 +12,37 @@ public class BordView extends Application{
 	public void start(Stage primaryStage) throws Exception {
 
 		BorderPane mainPane = new BorderPane();
-		Scene mainScene = new Scene(mainPane, 1280, 720);
+
 		mainPane.getStylesheets().add("Views/markup.css");
 		mainPane.setId("hallo");
-		
 
+		HBox stommeKaarten = new HBox();
+		HBox stommeKaarten2 = new HBox();
 
+		Pane kaart1 = new Pane();
+		Pane kaart2 = new Pane();
+		Pane kaart3 = new Pane();
+		Pane kaart4 = new Pane();
 
+		kaart1.setId("kaart1");
+		kaart2.setId("kaart2");
+		kaart3.setId("kaart3");
+		kaart4.setId("kaart4");
 
+		kaart1.setMinWidth(50);
+		kaart1.setMinHeight(50);
+		kaart2.setMinWidth(50);
+		kaart2.setMinHeight(50);
+		kaart3.setMinWidth(50);
+		kaart3.setMinHeight(50);
+		kaart4.setMinWidth(50);
+		kaart4.setMinHeight(50);
+
+	stommeKaarten.getChildren().addAll(kaart1, kaart2);
+	stommeKaarten2.getChildren().addAll(kaart3, kaart4);
+	mainPane.setLeft(stommeKaarten);
+	mainPane.setRight(stommeKaarten2);
+		Scene mainScene = new Scene(mainPane, 1280, 720);
 		primaryStage.setScene(mainScene);
 		primaryStage.show();
 	}
